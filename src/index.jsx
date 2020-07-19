9b09828f05561a5e72bc4412871dc0b9fbac599a
Araragi234 <johnsb234@gmail.com>
2020-07-19 11:21:53 -0500

    made hello world

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,24 @@
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter } from "react-router-dom";
 
-import App from "./App.js";
+// import App from "./App.js";
 
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById("app")
+// );
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App.jsx';
+ 
+const title = 'React with Webpack and Babel';
+ 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("app")
+  <App title={title} />,
+  document.getElementById('app')
 );
+
+module.hot.accept();
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/*
 const path = require('path');
 
 module.exports = {
@@ -7,4 +8,35 @@ module.exports = {
     filename: 'bundle.js'
   }
   module: {}
+};
+*/
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
 };
